d9f693ea0905c827db4f7f66fdbdc57e8864b5c3
Kira <gajdukkira232@gmail.com>
2026-03-17 16:12:39 +0200

    бстлділтмдфім

---
+++ TaskList/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import TaskList from './assets/TaskList'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <TaskList></TaskList>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ TaskList/src/assets/TaskList.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+class TaskList extends React.Component {
+
+  tasks = [
+    { id: 1, text: "Зайнятися спортом" },
+    { id: 2, text: "Зробити уроки" },
+    { id: 3, text: "Винести сміття" }
+  ];
+
+  deleteTask = (id) => {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+    this.forceUpdate();
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <h2>Список задач</h2>
+
+          <ul>
+            {this.tasks.map(task => (
+              <li key={task.id}>
+                {task.text}
+                <button onClick={() => this.deleteTask(task.id)}>
+                  Видалити
+                </button>
+              </li>
+            ))}
+          </ul>
+
+        </div>
+      </>
+    );
+  }
+}
+
+export default TaskList;
